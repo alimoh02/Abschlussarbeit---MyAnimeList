0e987f261771f3f525d80ab9a0eb82a0dcc4628b
alimoh02 <you@example.com>
2025-01-04 20:19:44 +0100

    Nur noch animes $ rviews verbinden

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,33 @@
 <h1>Welcome to MyAnimeList</h1>
 <p>Dein Portal des Vertrauens oder so!</p>
+
+<!-- GIF anzeigen -->
+<div class="welcome-gif">
+  <img src="/welcome.gif" alt="Welcome GIF" />
+</div>
+
+<style>
+  h1 {
+    text-align: center;
+    margin-top: 2rem;
+    color: #8afaa8;
+  }
+
+  p {
+    text-align: center;
+    font-size: 1.2rem;
+    color: #e0ffe0;
+  }
+
+  .welcome-gif {
+    display: flex;
+    justify-content: center;
+    margin-top: 2rem;
+  }
+
+  .welcome-gif img {
+    max-width: 100%;
+    height: auto;
+    border-radius: 10px;
+  }
+</style>
